b33e0879de8d7d89a349e98e50dca060b85cf509
LK <1455832457@qq.com>
2023-11-26 05:24:58 +0800

    发布

---
+++ src/Luke.AssemblyHotLoad/Luke.AssemblyHotLoad.Net45/IAssemblyLoadManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Luke.AssemblyHotLoad.Net45
+{
+    public interface IAssemblyLoadManager : IDisposable
+    {
+        Assembly GetAssembly(string assemblyName);
+
+        void StartWatcher();
+
+        void Add(string assemblyName);
+
+        void Remove(string assemblyName);
+    }
+}
