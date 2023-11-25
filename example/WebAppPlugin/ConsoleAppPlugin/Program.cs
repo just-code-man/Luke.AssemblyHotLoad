b0bfdb1c48fc9b6833b38e95893948d8e5ddff1d
LK <1455832457@qq.com>
2023-11-26 04:44:48 +0800

    添加.net4.5.2版本

---
+++ example/WebAppPlugin/ConsoleAppPlugin/Program.cs
@@ -0,0 +1,35 @@
+﻿using Luke.AssemblyHotLoad.Net45;
+using Plugin.Abstractions.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPlugin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IAssemblyLoadManager loadManager = new AssemblyLoadManager(new AssemblyLoadConfig {
+                Path = "Plugins", Filter = "Plugin.*.dll", AutoDelete = false
+            });
+            loadManager.StartWatcher();
+
+            string assemblyName = "Plugin.Test1.Framework";
+            string fullClassName = assemblyName + ".PluginCommand";
+            
+            while (true)
+            {
+                Assembly assembly = loadManager.GetAssembly(assemblyName);
+                IPluginCommand command = assembly.CreateInstance(fullClassName) as IPluginCommand;
+                command.Hello("use PluginCommand Hello, but not Reference Plugin.Test1.Framework");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ example/WebAppPlugin/Plugin.Test1.Framework/PluginCommand.cs
@@ -0,0 +1,17 @@
+﻿using Plugin.Abstractions.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin.Test1.Framework
+{
+    public class PluginCommand : IPluginCommand
+    {
+        public void Hello(string message)
+        {
+            Console.WriteLine("测试输出22" + message);
+        }
+    }
+}
